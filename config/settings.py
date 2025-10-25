792ebe6844e822acbec081efc34c68fe23a9a63f
Mario <89396504+Rakotondrainibe13@users.noreply.github.com>
2025-10-25 21:03:18 +0300

    Delete paramètre morte

--- config/settings.py
+++ config/settings.py
@@ -90,10 +90,5 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
 STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
